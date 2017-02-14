0ae5d09052a3ea2dfc19037baec365bd147f4285
Youngkyu <yk.hong@kaist.ac.kr>
2017-02-14 19:14:39 +0900

    basic functionality finished

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@
       </div>
     </div>
 
+
     <div id="addNewPhoto" class="ui modal">
       <i class="close icon"></i>
       <div class="header">
@@ -53,6 +54,7 @@
                 </div>
 
               </div>
+              <input type="color" id="html5colorpicker" @change="getColor($event)" value="#ff0000" style="width:85%;">
             </div>
           </div>
         </div>
@@ -70,10 +72,12 @@
       </div>
     </div>
 
+
     <div id="card_area">
       <div class="ui link cards">
         <div class="ui card"
-        v-for="(card, index) in cardlist" :key="card.id">
+        v-for="(card, index) in cardlist" :key="card.id"
+        @click="showDetail(card.title, card.description, card.image)">
         <div id="card_preview" class="image"
         v-bind:style="{ 'background-image': 'url(' + card.image + ')' }">
         <img/>
@@ -114,18 +118,41 @@
         card_title: "",
         card_description: "",
         card_photo_title: "",
-        card_photo: ""
+        card_photo: "",
+        rgb: "",
+        rgbhex: ""
       }
     },
     methods:{
-      uploadImage: function(){
+      getImages: function(){
         var httpPost = new XMLHttpRequest(),
-        path = "http://143.248.250.191:3000/uploadImage/",
-        data = JSON.stringify({title: this.card_title, description: this.card_description, image: this.card_photo});
+        path = "http://143.248.250.191:3000/getImages/",
+        data = JSON.stringify({});
         httpPost.onreadystatechange = function(err) {
           if (httpPost.readyState == 4 && httpPost.status == 200){
             var res = httpPost.responseText;
+            var jsonList = JSON.parse(res);
+            this.cardlist = jsonList;
             console.log(res);
+
+          } else {
+            console.log(err);
+          }
+        }.bind(this);
+        // Set the content type of the request to json since that's what's being sent
+        httpPost.open("POST", path, true);
+        httpPost.setRequestHeader('Content-Type', 'application/json');
+        httpPost.send(data);
+      },
+      uploadImage: function(){
+        var httpPost = new XMLHttpRequest(),
+        path = "http://143.248.250.191:3000/uploadImage/",
+        data = JSON.stringify({title: this.card_title, description: this.card_description, image: this.card_photo, rgb: this.rgb });
+        httpPost.onreadystatechange = function(err) {
+          if (httpPost.readyState == 4 && httpPost.status == 200){
+            var res = httpPost.responseText;
+            var jsonList = JSON.parse(res);
+            this.cardlist = jsonList;
             this.card_title = "";
             this.card_description = "";
             this.card_photo_title = "";
@@ -134,7 +161,7 @@
           } else {
             console.log(err);
           }
-        };
+        }.bind(this);
         // Set the content type of the request to json since that's what's being sent
         httpPost.open("POST", path, true);
         httpPost.setRequestHeader('Content-Type', 'application/json');
@@ -150,13 +177,35 @@
             this.card_description = "";
             this.card_photo_title = "";
 
-          },
+          }.bind(this),
           onShow: function(){
             this.card_title = "";
             this.card_description = "";
             this.card_photo_title = "";
             console.log('shown');
           },
+          onApprove: function() {
+            console.log('Approve');
+          }
+        }).modal('show');
+      },
+
+      showDetail: function(title, detail,photo_url){
+        console.log(title +"," +detail);
+        $('#seeDetail').modal({
+          onHide: function(){
+            console.log('hidden');
+            this.card_title = "";
+            this.card_description = "";
+            this.card_photo_title = "";
+
+          }.bind(this),
+          onShow: function(){
+            this.card_title = title;
+            this.card_description = detail;
+            $('#detail_image').attr('src', photo_url);
+            console.log('shown');
+          }.bind(this),
           onApprove: function() {
             console.log('Approve');
           }
@@ -174,6 +223,20 @@
         console.log(files);
         this.createImage(files[0]);
       },
+      getColor(e){
+        var color = e.target.value
+        var value = this.hexToRgb(color);
+        console.log(value);
+        this.rgb = value;
+      },
+      hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : null;
+      },
       createImage(file, index) {
         var image = new Image();
         var reader = new FileReader();
@@ -182,6 +245,9 @@
         };
         reader.readAsDataURL(file);
       },
+    },
+    mounted: function(){
+      this.getImages();
     }
   }
 </script>
@@ -211,7 +277,7 @@
   #info_title{
     font-size: 40px;
     font-weight: 700;
-    font-family: sans-serif;
+    font-family: 'Roboto';
   }
 
   .ui.secondary.pointing.menu{
@@ -225,12 +291,13 @@
 
   }
 
+
   #card_area{
     margin: 50px;
   }
 
   #card_preview{
-    width: 100%;
+    width: 290px;
     height: 300px;
     background-image: url('./assets/hhj1.jpg');
     background-size: 100% 100%;
